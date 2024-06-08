ef334f3d4e683ecf9c632892c70e2a1de89d8632
Ahmed Mansour <133324agh@gmail.com>
2024-06-08 10:20:55 +0300

    v2.0.1

---
+++ src/app/(userFlow)/user/settings/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+
+function page() {
+  return (
+    <div>
+        {/* user sttings page */}
+        <Image src={""} alt="">
+
+        </Image>
+    </div>
+  )
+}
+
+export default page
